c158cd65941b44b461395d028fef69ccb956e250
HandySandy123 <sweetiechimi@duck.com>
2026-04-12 04:38:41 +0200

    Prepping for full screen

--- UiMain.cs
+++ UiMain.cs
@@ -3,7 +3,32 @@ using System;
 
 public partial class UiMain : PanelContainer
 {
-	public void ButtonPressed(){
-		GD.Print("Button Pressed");
+	[Export]public TextureButton icePlanet;
+	[Export]public TextureButton waterPlanet;
+	[Export]public TextureButton RockPlanet;
+	[Export]public TextureButton duckPlanet;
+	[Export]public TextureButton frogButt;
+	[Export]public TextureButton hotBabesButt;
+	[Export]public TextureButton robButt;
+	[Export]public TextureButton ThoughtsRockButt;
+
+	private TextureButton[] planets;
+	private TextureButton[] PopulationButts;
+
+	public override void _EnterTree()
+	{
+		planets = new TextureButton[4] {icePlanet,waterPlanet,RockPlanet,duckPlanet};
+		PopulationButts = new TextureButton[4] {frogButt,hotBabesButt,robButt,ThoughtsRockButt};
+	}
+	
+	public void ButtonPressed()
+	{
+		foreach(var pop in PopulationButts)
+		{
+			if (pop.ButtonPressed)
+			{
+				
+			}
+		}	
 	}
 }
